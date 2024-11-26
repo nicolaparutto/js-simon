bdd70a2ef6ebd6ed908e3ecf52e4153d2cef1a2f
nicolaparutto <nicolaparutto2003@gmail.com>
2024-11-26 19:36:28 +0100

    feat: add end of exercise

--- JS/script.js
+++ JS/script.js
@@ -4,7 +4,7 @@ const countDownContainer = document.getElementById('random-n-container');
 const writeContainer = document.getElementById('write-n-container');
 
 //Sistema di countdown:
-let seconds = 10;
+let seconds = 2;
 
 countDown.innerHTML= seconds--;
 
@@ -42,6 +42,47 @@ liNum3.innerHTML = randNum3;
 liNum4.innerHTML = randNum4;
 liNum5.innerHTML = randNum5;
 
-//salvo i numeri generati in un array
+//salvo i numeri generati in un array:
 const arrayNumGenerati = [randNum1, randNum2, randNum3, randNum4, randNum5];
-console.log(arrayNumGenerati)
+
+
+//gestione sistema di confronto dei numeri inseriti con quelli generati:
+const num1 = document.getElementById('num1');
+const num2 = document.getElementById('num2');
+const num3 = document.getElementById('num3');
+const num4 = document.getElementById('num4');
+const num5 = document.getElementById('num5');
+
+const form = document.getElementById('form');
+
+form.addEventListener('submit', (event) => {
+   event.preventDefault();
+   const numeriInseriti = [
+      parseInt(num1.value), 
+      parseInt(num2.value), 
+      parseInt(num3.value), 
+      parseInt(num4.value), 
+      parseInt(num5.value)
+   ];
+
+   function returnArray (array1, array2){
+      return array1.filter(number => array2.includes(number));
+   }
+
+   //richiamo l'elemento in cui stamperò il risultato finale:
+   const finalResult = document.getElementById('final-result');
+   console.log(returnArray(arrayNumGenerati, numeriInseriti));
+   finalResult.innerHTML = `Hai indovinato questi numeri: ${returnArray(arrayNumGenerati, numeriInseriti)}`;
+
+
+   //reset valori inseriti:
+   function reset(){
+      num1.value = '';
+      num2.value = '';
+      num3.value = '';
+      num4.value = '';
+      num5.value = '';
+   }
+   //richiamo la funzione:
+   reset();
+});
